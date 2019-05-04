cad9f35c38663eefe773016de2a8f40b4dd2953a
Andrey Ivanov <andrey-tm203@mail.ru>
2019-05-04 16:24:35 +0300

    correct init function

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app.jsx';
 
-const init = () => {
-  const configs = {
-    gameTime: 5,
-    mistakesCount: 3,
-  };
+const configs = {
+  gameTime: 5,
+  mistakesCount: 3,
+};
 
+const init = () => {
   const main = document.querySelector(`.main`);
+
   ReactDOM.render(<App gameTime={configs.gameTime} mistakesCount={configs.mistakesCount} />, main);
 };
 
