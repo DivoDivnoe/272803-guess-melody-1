7ae58da98370d46c4155257ae04f094955f619e4
Andrey Ivanov <andrey-tm203@mail.ru>
2019-05-04 12:54:14 +0300

    change App component directory

--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import WelcomeScreen from './components/welcome-screen/welcome-screen.jsx';
-import PropTypes from 'prop-types';
-
-const App = (props) => {
-  const {gameTime, mistakesCount} = props;
-
-  return <WelcomeScreen gameTime={gameTime} mistakesCount={mistakesCount} />;
-};
-
-App.propTypes = {
-  gameTime: PropTypes.number,
-  mistakesCount: PropTypes.number,
-};
+import App from './components/app/app.jsx';
 
 const init = () => {
   const configs = {
