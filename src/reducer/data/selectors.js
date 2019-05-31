1fc760e688f765650fb00216c51ae0c56ea64630
Andrey Ivanov <andrey-tm203@mail.ru>
2019-05-31 19:32:31 +0300

    remove unnecessary selectors

--- src/reducer/data/selectors.js
+++ src/reducer/data/selectors.js
@@ -1,9 +1,5 @@
 import NameSpace from '../../name-spaces';
-import {createSelector} from 'reselect';
 
 const NAME_SPACE = NameSpace.DATA;
 
-export const getQuestions = createSelector(
-    (state) => state[NAME_SPACE].questions,
-    (questions) => questions
-);
+export const getQuestions = (state) => state[NAME_SPACE].questions;
--- src/reducer/game/selectors.js
+++ src/reducer/game/selectors.js
@@ -1,13 +1,6 @@
 import NameSpace from '../../name-spaces';
-import {createSelector} from 'reselect';
 
 const NAME_SPACE = NameSpace.GAME;
 
-export const getStep = createSelector(
-    (state) => state[NAME_SPACE].step,
-    (step) => step
-);
-export const getMistakes = createSelector(
-    (state) => state[NAME_SPACE].mistakes,
-    (mistakes) => mistakes
-);
+export const getStep = (state) => state[NAME_SPACE].step;
+export const getMistakes = (state) => state[NAME_SPACE].mistakes;
